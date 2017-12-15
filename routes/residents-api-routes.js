9fc4568e64a35d57e884eb923428734bf1154c8e
Manasi <manudee@gmail.com>
2017-12-15 17:28:32 -0500

    Added buttons and view handlebars for Residents packages

--- routes/residents-api-routes.js
+++ routes/residents-api-routes.js
@@ -25,8 +25,8 @@ module.exports = function(app) {
 	
 		
 			
-			let packages = {packages: dbPackage.Packages}
-
+			let packages = {packages: dbPackage.Packages};
+			// res.json(packages);
 			res.render('index', packages);
 
 		
