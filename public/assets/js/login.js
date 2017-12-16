5fca109efc1dfd2a03940f0926e62acf0c698ce5
Manasi <manudee@gmail.com>
2017-12-16 12:52:11 -0500

    Commented login js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,27 +1,32 @@
-$('#loginForm').submit(function(event){
-	console.log("hello!")
-	event.preventDefault();
-	
-	var userName= $("#username").val().trim();
-	var password= $("#password").val().trim();
 
-	console.log(userName);
-	console.log(password);
+// $(function() {
 
-	$.ajax({
+// $('#loginForm').submit(function(event){
+// 	console.log("hello!")
+// 	event.preventDefault();
+// 	$(this).data("id");
+// 	var userName= $data.val().trim();
+// 	var password= $("#password").val().trim();
 
-		url:'http://localhost:8081/authenticate',
-		method:"POST",
-		data: {
-				userName:userName,
-				password:password
+// 	console.log(userName);
+// 	console.log(password);
 
-		},
+// 	$.ajax({
 
-		dataType:'json'
-	})
+// 		url:'http://localhost:8081/authenticate',
+// 		method:"POST",
+// 		data: {
+// 				userName:userName,
+// 				password:password
+
+// 		},
+
+// 		dataType:'json'
+// 	})
 
 
 
 
-})
+// })
+
+// })
