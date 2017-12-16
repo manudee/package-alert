efd019c9e4043f3a809d04375f4ee37d6096fc01
Manasi <manudee@gmail.com>
2017-12-16 11:41:51 -0500

    Added resident block for handlebars and updated route links to match auth

--- routes/residents-api-routes.js
+++ routes/residents-api-routes.js
@@ -5,16 +5,13 @@ var pass = require('../config/passport/passport.js')(passport, db.User);
 module.exports = function(app) {
 
 
-// app.get('/api/residents', function(req,res){
-
-// 	console.log(req.user);
-	app.get("/landingPage/residents/packages/:id", function(req,res){
+	app.get("/api/packages/resident", function(req,res){
 		console.log("is resident api",req.user);
-	// req.params.id  = req.user.id;
+
 
 		db.User.find({
 			where: {
-				id: req.params.id
+				id: req.user.id
 			},
 			include:
 			{
@@ -22,15 +19,14 @@ module.exports = function(app) {
 			}
 
 		}).then(function(dbPackage){
-			//res.json(dbPackage);
-			console.log(dbPackage)
+
+		
 
 	
 		
 			
 			let packages = {packages: dbPackage.Packages};
-			// res.json(packages);
-			res.render('index', packages);
+			res.render('residents-block', packages);
 
 		
 
@@ -41,19 +37,26 @@ module.exports = function(app) {
 	});
 
 
-// })
 
 	app.put('/api/residents/:id/packages', function(req,res){
 
+
+		console.log("Request is " , req.body);
+		console.log("Request picked up date is " , req.body.pickUpDate);
+
+
+		console.log("Request status is " , req.body.status);
+		console.log("Req user is " , req.user.id)
+
 		db.Package.update({
-			pickUpDate: '12/14/2017',
-			status: 0
+			pickUpDate: req.body.pickUpDate,
+			status: req.body.status
 		},
 
 		{
 			where: {
 				userid: req.user.id,
-				id: 
+				id: req.params.id
 			}
 
 		}).then(function(updatedPackage){
