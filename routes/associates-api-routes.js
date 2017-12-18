940f00d178a643139ef5b693462b2e09c884dc6a
abhijit2908 <abhijitkghosh29@gmail.com>
2017-12-17 21:03:10 -0500

    create package dropdown working

--- routes/associates-api-routes.js
+++ routes/associates-api-routes.js
@@ -11,8 +11,9 @@ app.post("/api/create",function(req,res){
 	db.Package.create({
 		packageName :req.body.packageName,
 		creator:req.user.name,
-		UserId :req.body.Rid})
-	.then(function(result){	
+		UserId :req.body.residentList})
+	.then(function(result){
+		
 	
 		res.redirect('/api/packages/associate')
 	})
