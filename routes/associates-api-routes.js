ca43c359ce3b7f5485b91c70b344cc5a10736d4c
abhijit2908 <abhijitkghosh29@gmail.com>
2017-12-17 23:05:50 -0500

    removing unwanted files and folders attempt 2

--- routes/associates-api-routes.js
+++ routes/associates-api-routes.js
@@ -7,8 +7,6 @@ module.exports = function(app) {
 
 //ted
 app.post("/api/create",function(req,res){
-	// console.log("111",req)
-	//need to do something like db.Package.findAll then put it in the dropdown
 	db.Package.create({
 		packageName :req.body.packageName,
 		creator:req.user.name,
